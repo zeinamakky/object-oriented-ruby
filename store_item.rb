147518c937a484467b58e024b47e9e57ee8b385c
Zeina <zeemakky@gmail.com>
2016-02-29 21:00:56 -0600

    tea hashes

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+tea1 = {:variety => "english breakfast", :price => 2, :brand => "Bewleys"}
+tea2 = {:variety => "earl grey", :price => 3, :brand => "Ahmad"}
+tea3 = {variety: "jasmine green", price: 2, brand: "Rishi"}
+
+puts "I am selling #{tea1[:variety]} tea by #{tea1[:brand]} for $#{tea1[:price]}"
+puts "I am selling #{tea2[:variety]} tea by #{tea2[:brand]} for $#{tea2[:price]}"
+puts "I am selling #{tea3[:variety]} tea by #{tea3[:brand]} for $#{tea3[:price]}"
+puts " "
