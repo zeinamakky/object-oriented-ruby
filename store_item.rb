7005838427089bb31fa8f34b1ba2bc9cbf9a8144
Zeina <zeemakky@gmail.com>
2016-02-29 21:16:44 -0600

    made options hash for each tea instance

--- store_item.rb
+++ store_item.rb
@@ -8,10 +8,10 @@ puts "I am selling #{tea3[:variety]} tea by #{tea3[:brand]} for $#{tea3[:price]}
 puts " "
 class Tea 
   attr_reader :variety, :brand
-  def initialize(input_variety, input_price, input_brand)
-    @variety = input_variety
-    @price = input_price
-    @brand = input_brand
+  def initialize(input_options)
+    @variety = input_options[:variety]
+    @price = input_options[:price]
+    @brand = input_options[:brand]
   end
 
   def price
@@ -21,12 +21,19 @@ class Tea
   def print_info
     puts "I am selling #{@variety} tea by #{@brand} for $#{@price}"
   end
+
+  def increase_price
+    @price = @price * 2.10
+  end
 end
 
-tea1 = Tea.new("Earl Grey", 3, "Ahmad")
-tea2 = Tea.new("Irish Breakfast", 4, "Twinnings")
-tea3 = Tea.new("Jasmine Green", 6, "Teavana")
+tea1 = Tea.new(variety: "Earl Grey", price: 3, brand: "Ahmad")
+tea2 = Tea.new(variety: "Irish Breakfast", price: 4, brand: "Twinnings")
+tea3 = Tea.new(:variety => "Jasmine Green", :price => 6, :brand => "Teavana")
 
 tea1.print_info
 puts tea2.variety
 puts tea3.brand
+puts tea2.price
+tea2.increase_price
+puts "well now you have to pay #{tea2.price}, sucker"
