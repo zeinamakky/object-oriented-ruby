4b40583445c0482dae69c815194fddcfb1984ff7
Zeina <zeemakky@gmail.com>
2016-02-29 21:01:30 -0600

    made a tea class and made instances

--- store_item.rb
+++ store_item.rb
@@ -6,3 +6,27 @@ puts "I am selling #{tea1[:variety]} tea by #{tea1[:brand]} for $#{tea1[:price]}
 puts "I am selling #{tea2[:variety]} tea by #{tea2[:brand]} for $#{tea2[:price]}"
 puts "I am selling #{tea3[:variety]} tea by #{tea3[:brand]} for $#{tea3[:price]}"
 puts " "
+class Tea 
+  attr_reader :variety, :brand
+  def initialize(input_variety, input_price, input_brand)
+    @variety = input_variety
+    @price = input_price
+    @brand = input_brand
+  end
+
+  def price
+    @price
+  end
+ 
+  def print_info
+    puts "I am selling #{@variety} tea by #{@brand} for $#{@price}"
+  end
+end
+
+tea1 = Tea.new("Earl Grey", 3, "Ahmad")
+tea2 = Tea.new("Irish Breakfast", 4, "Twinnings")
+tea3 = Tea.new("Jasmine Green", 6, "Teavana")
+
+tea1.print_info
+puts tea2.variety
+puts tea3.brand
